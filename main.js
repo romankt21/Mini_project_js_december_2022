23ad0e62ba04add9b0bfee637b8e7cf9ed798870
romankt21 <romankt21@gmail.com>
2023-03-18 20:58:47 +0200

    ch

---
+++ main.js
@@ -0,0 +1,18 @@
+let usersArr = document.createElement('div');
+usersArr.classList.add('users');
+document.body.appendChild(usersArr);
+fetch('https://jsonplaceholder.typicode.com/users')
+    .then(value => value.json())
+    .then(users => users.forEach(user => {
+        let userDiv = document.createElement('div')
+        userDiv.classList.add('user');
+        let a = document.createElement('a');
+        a.innerText = `${user.id} ${user.name}`;
+        a.href = `./user-details.html?data=${JSON.stringify(user)}`;
+
+
+        usersArr.appendChild(userDiv)
+        userDiv.appendChild(a)
+
+
+    }))
